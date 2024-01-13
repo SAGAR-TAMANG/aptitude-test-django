52acdf8ffe92ddda4e9a95d006985d5abd7aec32
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-01-13 15:31:47 +0530

    latest

--- apt/settings.py
+++ apt/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-ur!69()z@f(alj!_&qc&uz-09$v=a6136!b%s&$min1^swb)-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+# DEBUG = os.environ.get("DEBUG")
+DEBUG = "True"
 
 ALLOWED_HOSTS = ["*"]
 
